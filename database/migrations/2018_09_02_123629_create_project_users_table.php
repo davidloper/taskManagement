dec91acbf2b13a65eb686469ca20f4eb90f0fbfd
david <david.ngu@beaconsbay.com>
2018-09-02 09:17:39 -0400

    added migrations files

--- database/migrations/2018_09_02_123629_create_project_users_table.php
+++ database/migrations/2018_09_02_123629_create_project_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateProjectUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,15 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('project_users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
             $table->integer('project_id');
-            $table->string('title');
-            $table->string('description',500);
-            $table->integer('assign_to');
-            $table->tinyInteger('priority');
-            $table->integer('duration_number');
-            $table->string('duration_type');
-            $table->string('status');
+            $table->integer('user_id');
+            $table->integer('user_level');
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      *
@@ -34,6 +29,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('project_users');
     }
 }
